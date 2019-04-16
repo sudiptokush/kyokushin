5dfa08509a96a504188bf0f38cf53097d98709e5
Sudipto Mukherjee <sudiptokush@gmail.com>
2019-04-16 19:56:56 +0530

    commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,11 +4,13 @@ import Layout from "../components/layout"
 export default () => (
     <Layout>
       <div className="body-content">
-        Tel: +91-33-23373259 (home)   +91-33-40044635 (office)
-        Fax: +91-33-28662055   Mob: +91-98-30025132 
-        BA - 175, Sector 1, Salt Lake City, Kolkata 700 064
-        e-mail: praseniko@yahoo.com 
-        URL: www.kyokushinkaikanindia.com
+        <ul>
+          <li>Tel: +91-33-23373259 (home)   +91-33-40044635 (office)</li>
+          <li>Fax: +91-33-28662055   Mob: +91-98-30025132 </li>
+          <li>BA - 175, Sector 1, Salt Lake City, Kolkata 700 064</li>
+          <li>e-mail: praseniko@yahoo.com</li>
+          <li>URL: www.kyokushinkaikanindia.com</li>
+        </ul>
       </div>
     </Layout>
   )
