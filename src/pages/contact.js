0b614dc044ad38853ee9f33ec5e54b13cb9aad7b
Sudipto Mukherjee <sudiptokush@gmail.com>
2019-04-24 18:03:10 +0530

    sudipto

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,17 +14,17 @@ export default () => (
             <li>Email: praseniko@yahoo.com</li>
             <li>Website: www.kyokushinkaikanindia.com</li>
           </ul>
-          <iframe title="map-location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.750815040541!2d88.39861759300794!3d22.58842131055852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0275ab2ef05531%3A0x6d7e5da4a6944350!2sKyokushin+Karate+Gurukul!5e0!3m2!1sen!2sin!4v1555671865675!5m2!1sen!2sin" allowfullscreen></iframe>
+          <iframe title="map-location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.750815040541!2d88.39861759300794!3d22.58842131055852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0275ab2ef05531%3A0x6d7e5da4a6944350!2sKyokushin+Karate+Gurukul!5e0!3m2!1sen!2sin!4v1555671865675!5m2!1sen!2sin" allowFullScreen></iframe>
         </div>
         <div className="long-tile-contact-form">
         <div className="contact-form-header">Contact Us</div>
-          <table class="contact-form">
+          <table className="contact-form">
            <tbody>
               <tr>
                 <td>
                     <label>Name <span>*</span></label>
                 </td>
-                <td class="entryColumn">
+                <td className="entryColumn">
                     <input type="text" required name="name" />
                 </td>
               </tr>
@@ -32,15 +32,15 @@ export default () => (
                 <td>
                     <label>Mobile <span>*</span></label>
                 </td>
-                <td class="entryColumn">
-                    <input type="text" pattern="[0-9]*" required name="mobile" minlength="10" maxlength="10" />
+                <td className="entryColumn">
+                    <input type="text" pattern="[0-9]*" required name="mobile" minLength="10" maxLength="10" />
                 </td>
               </tr>
               <tr>
                 <td>
                     <label>Email <span>*</span></label>
                 </td>
-                <td class="entryColumn">
+                <td className="entryColumn">
                     <input type="email" required pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$" name="email" />
                 </td>
               </tr>
@@ -53,8 +53,8 @@ export default () => (
                 </td>
               </tr>
               <tr>
-                <td colspan="2">
-                    <button type="submit" class="mailButton">Contact</button>
+                <td colSpan="2">
+                    <button type="submit" className="mailButton">Contact</button>
                 </td>
               </tr>
             </tbody>  
